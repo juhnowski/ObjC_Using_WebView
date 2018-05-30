42fd1ec4bcb92b1cc9a0b8dfac0acb338aee0d26
Juhnowski <juhnowski@gmail.com>
2018-05-31 02:34:23 +0300

    initial

--- Using_UIWebViews/ViewController.m
+++ Using_UIWebViews/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.sberbank.ru"]]];
 }
 
 
